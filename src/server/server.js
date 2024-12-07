72a0981b254ef2b99eb9ddca187e9fda3c510640
Ryan Rafael Nainggolan <ryanrafael032@gmail.com>
2024-12-07 14:39:55 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,60 +1,9 @@
-// require('dotenv').config();
-// const Hapi = require('@hapi/hapi');
-// const routes = require('../server/routes');
-// const loadModel = require('../services/loadModel');
-// const ClientError = require('../exceptions/ClientError');
- 
-// (async () => {
-//     const server = Hapi.server({
-//         port: process.env.PORT || 8080,
-//         host: '0.0.0.0',
-//         debug: { request: ['error'] },
-//         routes: {
-//             cors: {
-//               origin: ['*'],
-//             },
-//         },
-//     });
- 
-//     const model = await loadModel();
-//     server.app.model = model;
- 
-//     server.route(routes);
- 
-//     server.ext('onPreResponse', function (request, h) {
-//         const response = request.response;
- 
-//         if (response instanceof ClientError) {
-//             const newResponse = h.response({
-//                 status: 'fail',
-//                 message: `${response.message} Silakan gunakan foto lain.`
-//             })
-//             newResponse.code(response.statusCode)
-//             return newResponse;
-//         }
- 
-//         if (response.isBoom) {
-//             const newResponse = h.response({
-//                 status: 'fail',
-//                 message: response.message
-//             })
-//             newResponse.code(response.output.statusCode)
-//             return newResponse;
-//         }
- 
-//         return h.continue;
-//     });
- 
-//     await server.start();
-//     console.log(`Server start at: ${server.info.uri}`);
-// })();
-
-rrequire('dotenv').config();
+require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const loadModel = require('../services/loadModel');
-const ClientError = require('../exceptions/ClientError'); // Pastikan ini nama kelas yang benar
+const InputError = require('../exceptions/ClientError');
 
 (async () => {
   const server = Hapi.server({
@@ -75,7 +24,7 @@ const ClientError = require('../exceptions/ClientError'); // Pastikan ini nama k
   server.ext('onPreResponse', (request, h) => {
     const response = request.response;
 
-    if (response instanceof ClientError) {
+    if (response instanceof InputError) {
       return h
         .response({
           status: 'fail',
@@ -83,7 +32,7 @@ const ClientError = require('../exceptions/ClientError'); // Pastikan ini nama k
         })
         .code(response.statusCode);
     }
-    
+
     if (response.isBoom) {
       return h
         .response({
@@ -97,5 +46,6 @@ const ClientError = require('../exceptions/ClientError'); // Pastikan ini nama k
   });
 
   await server.start();
-  console.log(`Server started at: ${server.info.uri}`);
+  console.log(`Server start at: ${server.info.uri}`);
+  console.log(InputError);
 })();

736d99744a72336a9c4429f5fd1b3f12adadb8b1
Ryan Rafael Nainggolan <ryanrafael032@gmail.com>
2024-12-07 17:32:57 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -7,23 +7,23 @@ async function postPredictHandler(request, h) {
 	const { image } = request.payload;
 	const { model } = request.server.app;
 
-	const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
-	const id = crypto.randomUUID();
+	const { confidenceScore, label, suggestion } = await predictClassification(model, image);
+	const id        = crypto.randomUUID();
 	const createdAt = new Date().toISOString();
 
 	const data = {
 		id: id,
 		result: label,
-		explanation: explanation,
 		suggestion: suggestion,
 		confidenceScore: confidenceScore,
 		createdAt: createdAt,
 	};
 
+	await storeData(id, data);
 
 	const response = h.response({
 		status: "success",
-		message: confidenceScore > 99 ? "Model is predicted successfully." : "Model is predicted successfully but under threshold. Please use the correct picture",
+		message: confidenceScore > 0 ? "Model is predicted successfully" : "Please use the correct picture",
 		data,
 	});
 	response.code(201);
